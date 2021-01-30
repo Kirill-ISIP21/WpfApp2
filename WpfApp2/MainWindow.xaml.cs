d04cb9f73bd2f31dd9715d21668dbff8306de4bb
k.chukharev@mail.ru <k.chukharev@mail.ru>
2021-01-30 03:14:45 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProverka_Click(object sender, RoutedEventArgs e)
+        {
+            double znachenie;
+            double.TryParse(tBoxVvod.Text, out znachenie);
+            Num chislo = new Num(znachenie);
+            bool Znachenie = chislo.DvaZnach();
+            if (Znachenie)
+            {lblOtvet.Content = "Число является двузначным";}
+                else{lblOtvet.Content = "Число не является двузначным";}
+        }
+    }
+}
---
+++ WpfApp2/Num.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Num
+    {
+        public Num (double chislo)
+        {
+            this.chislo = chislo;
+        }
+        public double chislo { get; set; }
+        public bool DvaZnach() 
+        {
+            if (chislo > 9 && chislo < 100)
+                return true;
+            else return false;
+        }
+    }
+}
